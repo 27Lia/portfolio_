a83c217678402a8bf92dc700ee24a1c2b98d3c26
27Lia <117743861+27Lia@users.noreply.github.com>
2024-02-04 01:40:40 +0900

    style: 포로젝트 css 수정

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -11,48 +11,68 @@ import styled from "styled-components";
 function Portfolio() {
   const Div = styled.div`
     position: relative;
-    margin-bottom: 30px;
-    height: 250px;
+    height: 300px;
     background-color: #e6e4de;
     border-radius: 18px;
     width: 100%;
+    padding: 40px;
+    @media (max-width: 700px) {
+      padding: 0px;
+      height: auto;
+    }
   `;
 
   const Div2 = styled.div`
     position: relative;
-    margin-bottom: 30px;
     width: 100%;
-    height: 250px;
+    height: 300px;
     border-radius: 18px;
     background-color: #e9e9e9;
+    padding: 40px;
+    @media (max-width: 700px) {
+      height: auto;
+      padding: 0px;
+    }
   `;
 
   const Div22 = styled.div`
     position: relative;
-    margin-bottom: 30px;
     width: 100%;
-    height: 250px;
+    height: 300px;
     border-radius: 18px;
     background-color: #e9e9e9;
+    padding: 40px;
+    @media (max-width: 700px) {
+      height: auto;
+      padding: 0px;
+    }
   `;
 
   const Div3 = styled.div`
     position: relative;
-    margin-bottom: 30px;
-    height: 250px;
+    height: 300px;
     background-color: #f5da97;
     border-radius: 18px;
     width: 100%;
+    padding: 40px;
+    @media (max-width: 700px) {
+      height: auto;
+      padding: 0px;
+    }
   `;
 
   const Div4 = styled.div`
     position: relative;
     border-radius: 18px;
-    margin-bottom: 30px;
-    height: 250px;
+    height: 300px;
     width: 100%;
     background-color: #b9b9b9;
     border-radius: 18px;
+    padding: 40px;
+    @media (max-width: 700px) {
+      height: auto;
+      padding: 0px;
+    }
   `;
 
   const Img = styled.img`
@@ -71,7 +91,7 @@ function Portfolio() {
 
     @media (max-width: 700px) {
       flex-wrap: wrap;
-      gap: 0px;
+      gap: 50px;
     }
   `;
 
@@ -99,6 +119,7 @@ function Portfolio() {
   const Text = styled.div`
     font-size: 21px;
     color: #fff;
+    margin: 10px 0;
   `;
 
   return (
@@ -123,8 +144,8 @@ function Portfolio() {
             Repo
           </Button>
         </BtnBox>
-        <Text>NCT MBTI TEST</Text>
       </Div>
+      <Text>NCT MBTI TEST</Text>
 
       <SecondBox>
         <Div2 data-aos="fade-right" data-aos-duration="1000">
@@ -137,13 +158,13 @@ function Portfolio() {
               Repo
             </Button>
           </BtnBox>
-          <Text>하루모임 사이트</Text>
         </Div2>
 
         <Div22 data-aos="fade-left" data-aos-duration="1000">
           <Img src="../images/4.png" alt="포트폴리오 이미지"></Img>
         </Div22>
       </SecondBox>
+      <Text>하루모임 사이트</Text>
 
       <Div3 data-aos="fade-up" data-aos-duration="1000">
         <Img src="../images/2.png" alt="포트폴리오 이미지"></Img>
@@ -155,8 +176,8 @@ function Portfolio() {
             Repo
           </Button>
         </BtnBox>
-        <Text>여주 고철 웹페이지</Text>
       </Div3>
+      <Text>여주 고철 웹페이지</Text>
 
       <Div4 data-aos="fade-down" data-aos-duration="1000">
         <Img src="../images/5.png" alt="포트폴리오 이미지"></Img>
@@ -174,8 +195,8 @@ function Portfolio() {
             Repo
           </Button>
         </BtnBox>
-        <Text>쇼핑몰</Text>
       </Div4>
+      <Text>쇼핑몰</Text>
     </Container>
   );
 }
