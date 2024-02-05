05104722f20a544025a4bf20704a6fcb6f90ef0a
27Lia <117743861+27Lia@users.noreply.github.com>
2024-02-06 00:38:09 +0900

    style: 전체적인 css수정

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,69 +9,87 @@ import {
 import styled from "styled-components";
 
 function Portfolio() {
+  const DataContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+
+    @media (max-width: 700px) {
+    }
+  `;
+
   const Div = styled.div`
-    position: relative;
-    height: 300px;
+    height: 400px;
     background-color: #e6e4de;
     border-radius: 18px;
     width: 100%;
-    padding: 40px;
+    padding: 50px 30px;
+
     @media (max-width: 700px) {
-      padding: 0px;
+      padding: 20px;
       height: auto;
     }
+  `;
+
+  const DataBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 100%;
   `;
 
   const Div2 = styled.div`
     position: relative;
     width: 100%;
-    height: 300px;
+    height: 350px;
     border-radius: 18px;
     background-color: #e9e9e9;
-    padding: 40px;
+    padding: 20px 30px;
+
     @media (max-width: 700px) {
       height: auto;
-      padding: 0px;
+      padding: 20px;
     }
   `;
 
   const Div22 = styled.div`
     position: relative;
     width: 100%;
-    height: 300px;
+    height: 350px;
     border-radius: 18px;
     background-color: #e9e9e9;
-    padding: 40px;
+    padding: 50px 30px;
     @media (max-width: 700px) {
       height: auto;
-      padding: 0px;
+      padding: 20px;
     }
   `;
 
   const Div3 = styled.div`
     position: relative;
-    height: 300px;
+    height: 400px;
     background-color: #f5da97;
     border-radius: 18px;
     width: 100%;
-    padding: 40px;
+
+    padding: 50px 30px;
     @media (max-width: 700px) {
       height: auto;
-      padding: 0px;
+      padding: 20px;
     }
   `;
 
   const Div4 = styled.div`
     position: relative;
     border-radius: 18px;
-    height: 300px;
+    height: 400px;
     width: 100%;
     background-color: #b9b9b9;
     border-radius: 18px;
-    padding: 40px;
+    padding: 50px 30px;
     @media (max-width: 700px) {
       height: auto;
-      padding: 0px;
+      padding: 20px;
     }
   `;
 
@@ -79,7 +97,7 @@ function Portfolio() {
     width: 100%;
     height: 100%;
     border-radius: 18px;
-    object-fit: contain;
+    object-fit: cover;
   `;
 
   const SecondBox = styled.div`
@@ -87,11 +105,9 @@ function Portfolio() {
     align-items: center;
     justify-content: center;
     position: relative;
-    gap: 40px;
-
+    gap: 25px;
     @media (max-width: 700px) {
       flex-wrap: wrap;
-      gap: 50px;
     }
   `;
 
@@ -107,9 +123,6 @@ function Portfolio() {
   `;
 
   const BtnBox = styled.a`
-    bottom: 15px;
-    left: 10px;
-    position: absolute;
     display: flex;
     flex-wrap: wrap;
     height: auto;
@@ -119,7 +132,6 @@ function Portfolio() {
   const Text = styled.div`
     font-size: 21px;
     color: #fff;
-    margin: 10px 0;
   `;
 
   return (
@@ -133,70 +145,82 @@ function Portfolio() {
       <MainTitle>
         My <HighlightText>Projects</HighlightText>
       </MainTitle>
+      <DataContainer>
+        <Div data-aos="fade-right" data-aos-duration="1000">
+          <DataBox>
+            <Text>NCT MBTI TEST</Text>
+            <Img src="../images/1.png" alt="포트폴리오 이미지" />
+            <BtnBox>
+              <Button href="https://nctmbti.vercel.app/" target="_blank">
+                Site
+              </Button>
+              <Button target="_blank" href="https://github.com/27Lia/mbti">
+                Repo
+              </Button>
+            </BtnBox>
+          </DataBox>
+        </Div>
 
-      <Div data-aos="fade-right" data-aos-duration="1000">
-        <Img src="../images/1.png" alt="포트폴리오 이미지"></Img>
-        <BtnBox>
-          <Button href="https://nctmbti.vercel.app/" target="_blank">
-            Site
-          </Button>
-          <Button target="_blank" href="https://github.com/27Lia/mbti">
-            Repo
-          </Button>
-        </BtnBox>
-      </Div>
-      <Text>NCT MBTI TEST</Text>
+        <SecondBox>
+          <Div2 data-aos="fade-right" data-aos-duration="1000">
+            <DataBox>
+              <Text>하루모임 사이트</Text>
+              <Img src="../images/3.png" alt="포트폴리오 이미지"></Img>
+              <BtnBox>
+                <Button
+                  href="https://celebee-three.vercel.app/"
+                  target="_blank"
+                >
+                  Site
+                </Button>
+                <Button target="_blank" href="https://github.com/27Lia/celebee">
+                  Repo
+                </Button>
+              </BtnBox>
+            </DataBox>
+          </Div2>
 
-      <SecondBox>
-        <Div2 data-aos="fade-right" data-aos-duration="1000">
-          <Img src="../images/3.png" alt="포트폴리오 이미지"></Img>
-          <BtnBox>
-            <Button href="https://celebee-three.vercel.app/" target="_blank">
-              Site
-            </Button>
-            <Button target="_blank" href="https://github.com/27Lia/celebee">
-              Repo
-            </Button>
-          </BtnBox>
-        </Div2>
+          <Div22 data-aos="fade-left" data-aos-duration="1000">
+            <Img src="../images/4.png" alt="포트폴리오 이미지"></Img>
+          </Div22>
+        </SecondBox>
 
-        <Div22 data-aos="fade-left" data-aos-duration="1000">
-          <Img src="../images/4.png" alt="포트폴리오 이미지"></Img>
-        </Div22>
-      </SecondBox>
-      <Text>하루모임 사이트</Text>
+        <Div3 data-aos="fade-up" data-aos-duration="1000">
+          <DataBox>
+            <Text>여주 고철 웹페이지</Text>
+            <Img src="../images/2.png" alt="포트폴리오 이미지"></Img>
+            <BtnBox>
+              <Button href="https://junkshop.vercel.app/" target="_blank">
+                Site
+              </Button>
+              <Button target="_blank" href="https://github.com/27Lia/junkshop">
+                Repo
+              </Button>
+            </BtnBox>
+          </DataBox>
+        </Div3>
 
-      <Div3 data-aos="fade-up" data-aos-duration="1000">
-        <Img src="../images/2.png" alt="포트폴리오 이미지"></Img>
-        <BtnBox>
-          <Button href="https://junkshop.vercel.app/" target="_blank">
-            Site
-          </Button>
-          <Button target="_blank" href="https://github.com/27Lia/junkshop">
-            Repo
-          </Button>
-        </BtnBox>
-      </Div3>
-      <Text>여주 고철 웹페이지</Text>
-
-      <Div4 data-aos="fade-down" data-aos-duration="1000">
-        <Img src="../images/5.png" alt="포트폴리오 이미지"></Img>
-        <BtnBox>
-          <Button
-            href="https://fe-sprint-coz-shopping-eta.vercel.app/"
-            target="_blank"
-          >
-            Site
-          </Button>
-          <Button
-            target="_blank"
-            href="https://github.com/27Lia/fe-sprint-coz-shopping"
-          >
-            Repo
-          </Button>
-        </BtnBox>
-      </Div4>
-      <Text>쇼핑몰</Text>
+        <Div4 data-aos="fade-down" data-aos-duration="1000">
+          <DataBox>
+            <Text>쇼핑몰</Text>
+            <Img src="../images/5.png" alt="포트폴리오 이미지"></Img>
+            <BtnBox>
+              <Button
+                href="https://fe-sprint-coz-shopping-eta.vercel.app/"
+                target="_blank"
+              >
+                Site
+              </Button>
+              <Button
+                target="_blank"
+                href="https://github.com/27Lia/fe-sprint-coz-shopping"
+              >
+                Repo
+              </Button>
+            </BtnBox>
+          </DataBox>
+        </Div4>
+      </DataContainer>
     </Container>
   );
 }
