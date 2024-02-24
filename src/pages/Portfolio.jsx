7bd76490326037854badf4ffb9c10f45b4b79dc6
27Lia <117743861+27Lia@users.noreply.github.com>
2024-02-25 04:45:46 +0900

    style: 프로젝트배경변경

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -46,7 +46,7 @@ function Portfolio() {
 
   const Div = styled.div`
     height: 400px;
-    background-color: #e6e4de;
+    background-color: #e0e0e0;
     border-radius: 18px;
     width: 100%;
     padding: 50px 30px;
@@ -62,7 +62,7 @@ function Portfolio() {
     width: 100%;
     height: 350px;
     border-radius: 18px;
-    background-color: #e9e9e9;
+    background-color: #f9f5e7;
     padding: 50px 30px;
 
     @media (max-width: 700px) {
@@ -76,7 +76,7 @@ function Portfolio() {
     width: 100%;
     height: 350px;
     border-radius: 18px;
-    background-color: #e9e9e9;
+    background-color: #f9f5e7;
     padding: 50px 30px;
     @media (max-width: 700px) {
       height: auto;
@@ -87,7 +87,7 @@ function Portfolio() {
   const Div3 = styled.div`
     position: relative;
     height: 400px;
-    background-color: #f5da97;
+    background-color: #f5f5dc;
     border-radius: 18px;
     width: 100%;
 
@@ -103,7 +103,7 @@ function Portfolio() {
     border-radius: 18px;
     height: 400px;
     width: 100%;
-    background-color: #b9b9b9;
+    background-color: #404495;
     border-radius: 18px;
     padding: 50px 30px;
     @media (max-width: 700px) {
@@ -245,30 +245,7 @@ function Portfolio() {
             </LinkBox>
           </Text>
         </ProjectItem>
-        <Div3 data-aos="fade-up" data-aos-duration="1000">
-          <Img src="../images/2.png" alt="포트폴리오 이미지"></Img>
-        </Div3>
-        <ProjectItem>
-          <Text>
-            <PortfolioHeader>
-              <h3>여주 고철 웹페이지</h3>
-              <p>2023.09.30 ~ 2023.10.18</p>
-            </PortfolioHeader>
-            <p>회사소개 웹페이지 외주제작 프로젝트입니다.</p>
-            <ProjectText>
-              <p>- SEO 최적화</p>
-              <p>- 디자인 기획 및 반응형웹 구현, 슬라이드 구현</p>
-            </ProjectText>
-            <LinkBox>
-              <Button href="https://github.com/27Lia/junkshop" target="_blank">
-                <FaGithub />
-              </Button>
-              <a href="https://junkshop.vercel.app/" target="blank">
-                <FaLink />
-              </a>
-            </LinkBox>
-          </Text>
-        </ProjectItem>
+
         <Div4 data-aos="fade-down" data-aos-duration="1000">
           <Img src="../images/5.png" alt="포트폴리오 이미지"></Img>
         </Div4>
@@ -276,7 +253,7 @@ function Portfolio() {
           <Text>
             <PortfolioHeader>
               <h3>쇼핑몰</h3>
-              <p>2023.07 ~ 2023.08</p>
+              <p>2023.07 ~ 2023.08 (In development)</p>
             </PortfolioHeader>
             <p>Firebase를 사용한 간단한 쇼핑몰 웹 사이트입니다.</p>
             <ProjectText>
@@ -302,6 +279,31 @@ function Portfolio() {
                 href="https://27lia.github.io/fe-sprint-coz-shopping/"
                 target="blank"
               >
+                <FaLink />
+              </a>
+            </LinkBox>
+          </Text>
+        </ProjectItem>
+
+        <Div3 data-aos="fade-up" data-aos-duration="1000">
+          <Img src="../images/2.png" alt="포트폴리오 이미지"></Img>
+        </Div3>
+        <ProjectItem>
+          <Text>
+            <PortfolioHeader>
+              <h3>여주 고철 웹페이지</h3>
+              <p>2023.09.30 ~ 2023.10.18</p>
+            </PortfolioHeader>
+            <p>회사소개 웹페이지 외주제작 프로젝트입니다.</p>
+            <ProjectText>
+              <p>- SEO 최적화</p>
+              <p>- 디자인 기획 및 반응형웹 구현, 슬라이드 구현</p>
+            </ProjectText>
+            <LinkBox>
+              <Button href="https://github.com/27Lia/junkshop" target="_blank">
+                <FaGithub />
+              </Button>
+              <a href="https://junkshop.vercel.app/" target="blank">
                 <FaLink />
               </a>
             </LinkBox>
